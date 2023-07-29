bdd460a789f9d07c2f247c7d97a070866612a454
Prateek Srivastava <patrickdturner420@gmail.com>
2023-07-29 13:54:25 +0530

    changes

--- src/components/Aboutpage/Aboutpage.js
+++ src/components/Aboutpage/Aboutpage.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import '../../pages/style.css'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from "react";
+import "../../pages/style.css";
+import { Container, Row, Col } from "react-bootstrap";
 
-function Aboutpage () {
+function Aboutpage() {
   return (
-    <div className='aboutpagebackground'>
+    <div className="aboutpagebackground">
       <Container>
-        <Row className='textbackground'>
+        <Row className="textbackground">
           <Col md={7}>
-            <h3 className='aboutmetext'>
+            <h3 className="aboutmetext">
               About <span>Me</span>
             </h3>
-            <p className='aboutdetails'>
+            <p className="aboutdetails">
               I'm a self-taught Front End developer from India. I build websites
               with a focus on providing the experience for everyone using them
               and responsiveness. Curious to learn more about developing
               scalable distribution systems, love problem solving, and care
               about writing and maintainable code.
             </p>
-            <ul className='skilllist'>
+            <ul className="skilllist">
               <Row>
                 <h3>Skills</h3>
                 <Col md={7}>
@@ -29,6 +29,7 @@ function Aboutpage () {
                     <li>Bootsrap 5</li>
                     <li>React-Bootsrap</li>
                     <li>Material-ui</li>
+                    <li>Web3.JS</li>
                   </ul>
                 </Col>
                 <Col md={5}>
@@ -45,12 +46,12 @@ function Aboutpage () {
             </ul>
           </Col>
           <Col md={5}>
-            <div className='webimage'></div>
+            <div className="webimage"></div>
           </Col>
         </Row>
       </Container>
     </div>
-  )
+  );
 }
 
-export default Aboutpage
+export default Aboutpage;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,58 +1,58 @@
-import React, { useState } from 'react'
-import { Button } from 'react-bootstrap'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import { Link } from 'react-router-dom'
-import '../../pages/style.css'
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import "../../pages/style.css";
 
-function Header () {
-  const [expand, updateExpanded] = useState(false)
-  const [navColour, updateNavbar] = useState(false)
+function Header() {
+  const [expand, updateExpanded] = useState(false);
+  const [navColour, updateNavbar] = useState(false);
 
-  function scrollHandler () {
+  function scrollHandler() {
     if (window.scrollY >= 20) {
-      updateNavbar(true)
+      updateNavbar(true);
     } else {
-      updateNavbar(false)
+      updateNavbar(false);
     }
   }
 
-  window.addEventListener('scroll', scrollHandler)
+  window.addEventListener("scroll", scrollHandler);
 
   return (
     <Navbar
       expanded={expand}
-      fixed='top'
-      expand='md'
-      className={navColour ? 'sticky' : 'navbar'}
+      fixed="top"
+      expand="md"
+      className={navColour ? "sticky" : "navbar"}
     >
-      <Navbar.Brand className='logotext' as={Link} to='/'>
-        <div className='logo'></div>
+      <Navbar.Brand className="logotext" as={Link} to="/">
+        <div className="logo"></div>
       </Navbar.Brand>
 
       <Navbar.Toggle
-        className='navbar-toggler'
-        aria-controls='responsive-navbar-nav'
+        className="navbar-toggler"
+        aria-controls="responsive-navbar-nav"
         onClick={() => {
-          updateExpanded(expand ? false : 'expanded')
+          updateExpanded(expand ? false : "expanded");
         }}
       >
         <span></span>
         <span></span>
         <span></span>
       </Navbar.Toggle>
-      <Navbar.Collapse id='responsive-navbar-nav' className='responsive-navbar'>
-        <Nav className='ms-auto' defaultActiveKey='#home'>
+      <Navbar.Collapse id="responsive-navbar-nav" className="responsive-navbar">
+        <Nav className="ms-auto" defaultActiveKey="#home">
           <Nav.Item>
-            <Nav.Link as={Link} to='/' onClick={() => updateExpanded(false)}>
-              {' '}
-              Home{' '}
+            <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
+              {" "}
+              Home{" "}
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
             <Nav.Link
               as={Link}
-              to='/about'
+              to="/about"
               onClick={() => updateExpanded(false)}
             >
               About
@@ -72,7 +72,7 @@ function Header () {
           <Nav.Item>
             <Nav.Link
               as={Link}
-              to='/contact'
+              to="/contact"
               onClick={() => updateExpanded(false)}
             >
               Contact
@@ -82,17 +82,17 @@ function Header () {
           <Button
             onClick={() => {
               window.open(
-                'https://drive.google.com/file/d/1moxezyN4ljQDQzfuBQg6gCaC4Q_Ewf27/view'
-              )
+                "https://docs.google.com/document/d/1OvbR7tbwQfW4N0WXNRC-RIarA1JiMlY7dQc162THUKI/edit?usp=drive_link"
+              );
             }}
-            className='resumebtn'
+            className="resumebtn"
           >
             <span>Resume</span>
           </Button>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
 
-export default Header
+export default Header;
